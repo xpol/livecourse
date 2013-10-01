cd1fa21e57e0ced66062d896f538e887ef6ec5a1
xpol <xpol@localhost>
2013-10-01 22:39:31 +0800

    Lessons page tabs.

--- routes/lesson.js
+++ routes/lesson.js
@@ -10,18 +10,14 @@ var lessons = JSON.parse(fs.readFileSync('db/lessons/all.json'))
 exports.index = function(req, res){
   var id = req.params.course
   console.log("handling lessons for course "+id)
-
-  function render(){
-    res.render('lessons', { path:'/lessons', course:id, lesson:lesson });
-  }
-
+  
   var lesson = lessons.all[1]
   
   fs.readFile('db/lessons/'+lesson.file, function(err, data){
     console.log("Lading lesson file: " + lesson.file);
     if (err) throw err;
     lesson.data = data
-    render();
+    res.render('lessons', { path:'/lessons', course:id, lesson:lesson });
   }); 
 };
 
